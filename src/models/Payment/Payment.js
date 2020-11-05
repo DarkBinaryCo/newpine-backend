f88dd40bec066dedfd7b8b8263a0a7982171148d
AllanJeremy <aj.dev254@gmail.com>
2020-11-05 18:30:59 +0300

    [Enhancement] Added paymentID - Autogenerated UUID

--- src/models/Payment/Payment.js
+++ src/models/Payment/Payment.js
@@ -1,45 +1,49 @@
-const db = require('../../database');
-const {
-    DataTypes
-} = require('sequelize');
+const db = require("../../database");
+const { DataTypes, Sequelize } = require("sequelize");
 
 /** Attributes expected in the database */
 const _paymentAttributes = {
-    userId: {
-        type: DataTypes.INTEGER(10).UNSIGNED,
-        allowNull: false
-    },
-    amount: {
-        type: DataTypes.FLOAT,
-        allowNull: false
-    },
-    confirmationCode: {
-        type: DataTypes.STRING(128),
-        allowNull: true
-    },
-    paymentMethod: {
-        type: DataTypes.ENUM('mpesa', 'card'),
-        allowNull: false,
-        defaultValue: 'mpesa'
-    },
-    paymentType: {
-        type: DataTypes.ENUM('servicePayment', 'providerPayout', 'serviceRefund'),
-        allowNull: false,
-        defaultValue: 'servicePayment'
-    },
-    isConfirmed: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-    },
-    dateConfirmed: {
-        type: DataTypes.DATE,
-        allowNull: true
-    }
-}
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  confirmationCode: {
+    type: DataTypes.STRING(128),
+    allowNull: true,
+  },
+  paymentMethod: {
+    type: DataTypes.ENUM("mpesa", "card"),
+    allowNull: false,
+    defaultValue: "mpesa",
+  },
+  paymentType: {
+    type: DataTypes.ENUM("servicePayment", "providerPayout", "serviceRefund"),
+    allowNull: false,
+    defaultValue: "servicePayment",
+  },
+  isConfirmed: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  dateConfirmed: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+};
 
 /** Schema definition */
-const Payment = db.define('payment', _paymentAttributes);
+const Payment = db.define("payment", _paymentAttributes);
 
 //* EXPORTS
 module.exports = Payment;
