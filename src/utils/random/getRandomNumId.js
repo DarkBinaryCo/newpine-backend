82f581cdf555fa9d874a71f10cd76e48498b30f7
AllanJeremy <aj.dev254@gmail.com>
2020-11-05 18:09:40 +0300

    [Bugfix] Fixed reference error in random number utils

--- src/utils/random/getRandomNumId.js
+++ src/utils/random/getRandomNumId.js
@@ -1,16 +1,17 @@
 // Config
-const {
-    USER_ID_VALUE
-} = require('../../config/constants');
+const { USER_ID_VALUE } = require("../../config/constants");
 
 // Internal - Random util related
-const getRandomNumInRange = require('./getRandomNumInRange');
+const getRandomNumInRange = require("./getRandomNumInRange");
 
-/** Get a random integer id. Uses timestamp and random prefix & suffix to generate an integer id */
-const getRandomNumId = () => {
-    const generatedId = getRandomNumInRange(USER_ID_VALUE.MIN, USER_ID_VALUE.MAX);
-    return generatedId;
-}
+/** Get a random integer id. Uses timestamp and random prefix & suffix to generate an integer id
+ * @param {Number} min The minimum id number
+ * @param {Number} max The maximum id number
+ */
+const getRandomNumId = (min = 10000, max = 999999999) => {
+  const generatedId = getRandomNumInRange(min, max);
+  return generatedId;
+};
 
 //* EXPORTS
 module.exports = getRandomNumId;
