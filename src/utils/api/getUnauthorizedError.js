35e0b654ca51044ec3ce82765fa4917095331898
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 18:43:21 +0300

    [Documentation] Updated getUnauthorizedError comment

--- src/utils/api/getUnauthorizedError.js
+++ src/utils/api/getUnauthorizedError.js
@@ -1,7 +1,7 @@
 // Internal
 const getError = require("./getError");
 
-/** Get an error response
+/** Get an unauthorized error response. Ideally used when a user does not have permission to access a resource.
  * @param {String} message The error message to return as part of the response
  * @param {Object<Error>} error The error object containing the error's data
  * @return {Object} An API response object
