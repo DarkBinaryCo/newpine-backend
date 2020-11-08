ff5dd1f3b73eb0439b34b9bece0fdb44f6233c09
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 22:52:42 +0300

    [Migration] Added Master migration file - Migrates all models in order, waiting for one model to sync before attempting to sync the next one

---
+++ src/migrations/Master.js
@@ -0,0 +1,29 @@
+// Listed in the order they should be created in
+const {
+  UserType,
+  User,
+  AuthToken,
+  PropertyType,
+  PropertyGroupType,
+  PropertyGroup,
+  Property,
+  Payment,
+} = require("../models");
+
+//* This migration syncs all models ~ creating every table in the database
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Order matters, so we can set the relationships correctly
+    await UserType.sync();
+    await User.sync();
+    await AuthToken.sync();
+    await PropertyType.sync();
+    await PropertyGroupType.sync();
+    await PropertyGroup.sync();
+    await Property.sync();
+    await Payment.sync();
+  },
+  down: (queryInterface, Sequelize) => {
+    queryInterface.dropDatabase();
+  },
+};
