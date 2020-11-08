65dd127d7a675db071ced45fa819182e63c6383c
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 13:22:05 +0300

    [Bugfix] Fixed issue with _getTokenDataOfUserType

--- src/middleware/PermissionMiddleware/_getTokenDataOfUserType.js
+++ src/middleware/PermissionMiddleware/_getTokenDataOfUserType.js
@@ -1,34 +1,32 @@
-const {
-    AuthService
-} = require('../../services');
+const { AuthService } = require("../../services");
 
 /** [Helper] Get token information and the user that owns the token
  * @param {Object} req The request from a middleware. Should contain a bearer token.
  * @return {Object|null} The token owner if found and null if no owner was found for that token
  */
 const _getTokenDataOfUserType = async (req, userTypeIdEntered) => {
-    // Set response data defaults ~ returned before we find user
-    let responseData = {
-        loggedIn: false,
-        tokenData: null
-    };
+  // Set response data defaults ~ returned before we find user
+  let responseData = {
+    loggedIn: false,
+    tokenData: null,
+  };
 
-    // No need to go further if we don't have any authorization headers provided
-    if (!req.headers.authorization) return 'responseData';
+  // No need to go further if we don't have any authorization headers provided
+  if (!req.headers.authorization) return responseData;
 
-    // Get the token
-    const token = (req.headers.authorization.split(" "))[1];
-    const tokenOwner = await AuthService.getAuthToken(token);
+  // Get the token
+  const token = req.headers.authorization.split(" ")[1];
+  const tokenOwner = await AuthService.getAuthToken(token);
 
-    console.log("Token owner: ", tokenOwner);
-    if (!tokenOwner || !tokenOwner.user) return responseData;
+  console.log("Token owner: ", tokenOwner);
+  if (!tokenOwner || !tokenOwner.user) return responseData;
 
-    //* Getting here means that we found a token owner
-    responseData.loggedIn = tokenOwner.user.userTypeId === userTypeIdEntered;
-    responseData.tokenData = tokenOwner;
+  //* Getting here means that we found a token owner
+  responseData.loggedIn = tokenOwner.user.userTypeId === userTypeIdEntered;
+  responseData.tokenData = tokenOwner;
 
-    return responseData;
-}
+  return responseData;
+};
 
 //* EXPORTS
 module.exports = _getTokenDataOfUserType;
