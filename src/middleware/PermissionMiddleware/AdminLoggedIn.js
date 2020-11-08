543567260fb9c3f0d1c390e6edb6bec53b35832f
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 15:07:23 +0300

    [Refactoring] Consolidated relevant code into the function - Instead of having the AUTH_DATA constant as a "global" variables, we have moved it into the method using it - Removed return statement. We are actually not using the returned data but rather data set inside the function (Clearly not a recommended thing to do - mutability leads to unforeseen bugs)

--- src/middleware/PermissionMiddleware/AdminLoggedIn.js
+++ src/middleware/PermissionMiddleware/AdminLoggedIn.js
@@ -1,22 +1,21 @@
-//? Duplicate this  file and change the appropriate user
+//? Duplicate this  file and change `AUTH_DATA` to the appropriate user type
 const UserLoggedIn = require("./UserLoggedIn");
 
 // Config
 const { USER_TYPE } = require("../../config/auth");
-
-const AUTH_DATA = {
-  userTypeId: USER_TYPE.ADMIN,
-  userTypeTitle: "admin",
-};
 
 /** Only proceeds to the next middleware if a sales user is logged in based on the `AuthToken` passed in.
  *
  * Also sets `userData` on the `request` as `req.userData` that can be used by any middleware that comes after
  */
 const AdminLoggedIn = async (req, res, next) => {
-  req.authData = AUTH_DATA;
+  const AUTH_DATA = {
+    userTypeId: USER_TYPE.ADMIN,
+    userTypeTitle: "admin",
+  };
 
-  return UserLoggedIn(req, res, next);
+  req.authData = AUTH_DATA;
+  UserLoggedIn(req, res, next);
 };
 
 //* EXPORTS
