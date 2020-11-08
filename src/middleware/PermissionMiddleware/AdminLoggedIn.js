44240282075a3a15aca26255cd901da03dd60d09
AllanJeremy <aj.dev254@gmail.com>
2020-11-08 18:38:42 +0300

    [Feature] AdminLoggedIn functionality is now working as expected - This middleware must be called after the UserLoggedIn middleware as it relies on it to get the logged in user's information.
    - This separation of concerns (having different middleware for both) also allows us to process the user information and return extra data if necessary (in this case this could be extra admin related information)

--- src/middleware/PermissionMiddleware/AdminLoggedIn.js
+++ src/middleware/PermissionMiddleware/AdminLoggedIn.js
@@ -1,7 +1,14 @@
-/* For this file  to work, the user must be logged in (MUST be used after calling the UserLoggedIn middleware) */
+// Utils
+const {
+  getApiUnauthorizedError,
+  printApiResponse,
+} = require("../../utils/api");
+
+//! For this file  to work, the user must be logged in (MUST be used after calling the UserLoggedIn middleware)
 //? Duplicate this  file and change `AUTH_DATA` to the appropriate user type
 // Config
 const { USER_TYPE } = require("../../config/auth");
+const printResponse = require("../../utils/api/printResponse");
 
 /** Only proceeds to the next middleware if a sales user is logged in based on the `AuthToken` passed in.
  *
@@ -15,12 +22,14 @@ const AdminLoggedIn = async (req, res, next) => {
     throw new Error(
       "\nIt would appear the middleware was called before calling UserLoggedIn. Please call the UserLoggedIn middleware before calling this.\n"
     );
+  } else if (userData.userTypeId === USER_TYPE.ADMIN) {
+    console.debug("Admin is logged in");
+    // Admin is indeed logged in
+    next();
+  } else {
+    let apiResponse = getApiUnauthorizedError();
+    printApiResponse(res, apiResponse, next);
   }
-
-  console.log("\nUser data: ", userData);
-  // USER_TYPE.ADMIN;
-
-  console.log(req);
 };
 
 //* EXPORTS
