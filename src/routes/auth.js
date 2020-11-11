f12ac9ef831f3fd7bd908f57657dfb72435eec2a
AllanJeremy <aj.dev254@gmail.com>
2020-11-11 20:15:53 +0300

    [Formatting] Cleaned up auth routes formatting

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    AuthMiddleware
-} = require("../middleware");
+const { AuthMiddleware } = require("../middleware");
 
 // Send OTP
-router.post('/otp', AuthMiddleware.SendOtp);
+router.post("/otp", AuthMiddleware.SendOtp);
 
 // Verify OTP
-router.post('/otp/verify', AuthMiddleware.VerifyOtp);
+router.post("/otp/verify", AuthMiddleware.VerifyOtp);
 
 //* EXPORTS
 module.exports = router;
