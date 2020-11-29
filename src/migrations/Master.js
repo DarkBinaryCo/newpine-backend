4a7e3665d97ed040d96721fd7313077a35419039
AllanJeremy <aj.dev254@gmail.com>
2020-11-29 14:22:32 +0300

    [Database] Updated master migration to include the newly added models The models have also been added in the appropriate order

--- src/migrations/Master.js
+++ src/migrations/Master.js
@@ -1,5 +1,6 @@
 // Listed in the order they should be created in
 const {
+  IdentificationTypes,
   UserType,
   User,
   AuthToken,
@@ -9,12 +10,15 @@ const {
   Property,
   Payment,
   Resident,
+  Vehicle,
+  VisitorInvitation,
 } = require("../models");
 
 //* This migration syncs all models ~ creating every table in the database
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // Order matters, so we can set the relationships correctly
+    await IdentificationTypes.sync();
     await UserType.sync();
     await User.sync();
     await AuthToken.sync();
@@ -24,6 +28,8 @@ module.exports = {
     await Property.sync();
     await Payment.sync();
     await Resident.sync();
+    await Vehicle.sync();
+    await VisitorInvitation.sync();
   },
   down: (queryInterface, Sequelize) => {
     queryInterface.dropDatabase();
