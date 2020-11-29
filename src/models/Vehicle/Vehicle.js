1a2049f0b00d73460e78e7897df35402f6894a73
AllanJeremy <aj.dev254@gmail.com>
2020-11-29 14:20:43 +0300

    [Database] Added vehicles model

---
+++ src/models/Vehicle/Vehicle.js
@@ -0,0 +1,48 @@
+const sequelize = require("../../database");
+const { DataTypes, Model, Sequelize } = require("sequelize");
+
+const { Resident } = require("../Resident");
+//
+class Vehicle extends Model {}
+
+// Schema definition
+Vehicle.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    numberPlate: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+      defaultValue: null,
+    },
+    residentId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "residents", //? Table name
+        key: "id",
+      },
+    },
+    isVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    verifierUserId: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      defaultValue: null,
+    },
+  },
+  { sequelize, modelName: "Vehicle" }
+);
+
+// Relationships
+Vehicle.belongsTo(Resident, { foreignKey: "residentId" });
+
+//* EXPORTS
+module.exports = Vehicle;
