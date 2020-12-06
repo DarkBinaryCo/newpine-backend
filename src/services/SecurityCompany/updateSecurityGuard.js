081d45f07266a1e4848cae99f4c814e30e815d4d
AllanJeremy <aj.dev254@gmail.com>
2020-12-06 14:15:07 +0300

    [Documentation] Clarified some documentation for updateSecurityGuard

--- src/services/SecurityCompany/updateSecurityGuard.js
+++ src/services/SecurityCompany/updateSecurityGuard.js
@@ -1,9 +1,9 @@
 // Models
 const { SecurityGuard } = require("../../models");
 
-/** Update a user matching the `filter` with `updateData`
+/** Update a Security guard matching the `filter` with `updateData`. This can only be used by security managers. Security guards can only update the user part of their account
  * @param {Object} updateData Database information to update
- * @param {Object} filter User filter
+ * @param {Object} filter Security guard filter
  * @return {Promise<Object>} A promise that resolves to an object with the update operation information
  */
 const updateSecurityGuard = async (updateData = {}, filter = {}) => {
