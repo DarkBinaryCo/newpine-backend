0e823e991e6af728fb92fed05bc685b1e9f9681e
AllanJeremy <aj.dev254@gmail.com>
2020-12-07 19:29:09 +0300

    [Routing] Added admin and security routes to route aggregator

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 // Routes
 const AuthRoutes = require("./routes/auth");
+const AdminRoutes = require("./routes/admin");
 const ResidentRoutes = require("./routes/resident");
+const SecurityCompanyRoutes = require("./routes/security-company");
 
 //* Official route setup
 router.use("/auth", AuthRoutes);
+router.use("/admin", AdminRoutes);
 router.use("/resident", ResidentRoutes);
+router.use("/security", SecurityCompanyRoutes);
 
 module.exports = router;
