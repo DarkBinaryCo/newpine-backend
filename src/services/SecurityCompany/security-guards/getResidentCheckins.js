09e58b8951a20ce8236ba35b2ebaa0a8ed84d6c0
AllanJeremy <aj.dev254@gmail.com>
2020-12-08 17:50:12 +0300

    [Feature] Implemented get multiple & single resident checkins

---
+++ src/services/SecurityCompany/security-guards/getResidentCheckins.js
@@ -0,0 +1,23 @@
+const { ResidentCheckin, Vehicle } = require("../../../models");
+
+/** Get resident checkins by a filter
+ * @param {Object} filter Resident checkin query filter
+ * @param {Boolean} isAdmin Whether the caller of the function is an admin or not
+ */
+const getResidentCheckins = async (filter = {}, isAdmin = false) => {
+  let findOptions = {
+    where: filter,
+    include: {
+      model: Vehicle,
+      required: true,
+      /* attributes: [] */
+    },
+    paranoid: !isAdmin, //? Only admins can see all records (including paranoid deleted ones)
+    // attributes: attributesToInclude,
+  };
+
+  return ResidentCheckin.findAll(findOptions);
+};
+
+//* EXPORTS
+module.exports = getResidentCheckins;
