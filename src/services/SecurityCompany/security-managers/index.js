4482a1412802673c807d557fd3fb1601be349e97
AllanJeremy <aj.dev254@gmail.com>
2020-12-08 17:53:33 +0300

    [Documentation] Updated comment in security company security manager aggregator

--- src/services/SecurityCompany/security-managers/index.js
+++ src/services/SecurityCompany/security-managers/index.js
@@ -1,5 +1,5 @@
 /* 
-    This file is simply meant to be an aggregator of all SecurityCompanyService/managers related functions.
+    This file is simply meant to be an aggregator of all SecurityCompanyService/security-managers related functions.
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 const createSecurityGuard = require("./createSecurityGuard");
