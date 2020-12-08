199430f0a8d026c3176b9706a4c636c4ff460957
AllanJeremy <aj.dev254@gmail.com>
2020-12-08 14:00:14 +0300

    [Refactoring] Renamed middleware directory name to a pluralized version : security-manager was updated to security-managers References to the files were also changed to reflect these naming changes.

--- src/middleware/SecurityCompanyMiddleware/index.js
+++ src/middleware/SecurityCompanyMiddleware/index.js
@@ -2,7 +2,7 @@
     This file is simply meant to be an aggregator of all SecurtyCompanyMiddleware related functions.
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
-const SecurityManagerFunctions = require("./security-manager");
+const SecurityManagerFunctions = require("./security-managers");
 // const SecurityGuardFunctions = require("./security-guard");
 
 //* EXPORTS
