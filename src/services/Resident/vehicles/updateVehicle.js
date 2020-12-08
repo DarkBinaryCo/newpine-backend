fa4ca9b7c1cbaf525e67b0624a57c672c1408377
AllanJeremy <aj.dev254@gmail.com>
2020-12-08 17:12:03 +0300

    [Documentation] Update vehicle function description

--- src/services/Resident/vehicles/updateVehicle.js
+++ src/services/Resident/vehicles/updateVehicle.js
@@ -3,7 +3,6 @@ const { Vehicle } = require("../../../models");
 /** Update a vehicle
  * @param {Object} updateData Update data
  * @param {Object} filter Vehicle filter
- * @param {Boolean} isResidentRep Whether the person attempting to update is a resident representative (resident admin)
  * @return {Promise<Object>} A promise that resolves to an object with the update operation information
  */
 const updateVehicle = async (updateData, filter = {}) => {
