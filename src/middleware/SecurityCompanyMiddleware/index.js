bae56b72dc40469ac2e20abefbd9dd30b9f74194
AllanJeremy <aj.dev254@gmail.com>
2020-12-10 10:12:40 +0300

    [Enhancement] Added security guard middleware functions to securty company middleware aggregator

--- src/middleware/SecurityCompanyMiddleware/index.js
+++ src/middleware/SecurityCompanyMiddleware/index.js
@@ -3,10 +3,10 @@
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 const SecurityManagerFunctions = require("./security-managers");
-// const SecurityGuardFunctions = require("./security-guard");
+const SecurityGuardFunctions = require("./security-guards");
 
 //* EXPORTS
 module.exports = {
   ...SecurityManagerFunctions,
-  // ...SecurityGuardFunctions,
+  ...SecurityGuardFunctions,
 };
