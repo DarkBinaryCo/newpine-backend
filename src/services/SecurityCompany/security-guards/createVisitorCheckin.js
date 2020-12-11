28106f610aa147f6d5a568d7ad5028591ce72fac
AllanJeremy <aj.dev254@gmail.com>
2020-12-11 08:26:24 +0300

    [Enhancement] De-activating visitor invitation when they check-in

--- src/services/SecurityCompany/security-guards/createVisitorCheckin.js
+++ src/services/SecurityCompany/security-guards/createVisitorCheckin.js
@@ -1,11 +1,22 @@
 const { VisitorCheckin } = require("../../../models");
 
+// Services
+//! Find a way to use the services aggregator
+const VisitorService = require("../../../services/Visitor");
+
 /** Create a resident checkin
  * @param {Object} insertData Data to be inserted into the database
  * @return {Promise<Object>} A promise that resolves to an object with the create operation information
  */
 const createVisitorCheckin = async (insertData) => {
   let settableFields = ["visitorInvitationId", "securityGuardId", "isCheckin"];
+
+  // De-activate the visitor invitation as soon as they have been checked-in
+  let updateFilter = { id: insertData.visitorInvitationId };
+  let updateData = { isActive: false };
+
+  //? De-activating means the visitor can be checked out but they cannot use the same invitation to get in
+  VisitorService.updateVisitorInvitation(updateData, updateFilter);
 
   return VisitorCheckin.create(insertData, {
     fields: settableFields,
