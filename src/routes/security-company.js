f0303c109f52d3224dd49eb34b7f8045c0dc72b5
AllanJeremy <aj.dev254@gmail.com>
2020-12-12 09:13:46 +0300

    [Routes] Added security company routes

--- src/routes/security-company.js
+++ src/routes/security-company.js
@@ -8,13 +8,22 @@ const {
 } = require("../middleware");
 
 //* SECURITY MANAGER FUNCTIONALITY
-// Add security guards
+// Add security guard user accounts
 router.post(
-  "/manager/security-guards",
+  "/manager/security-guard-users",
   UtilityMiddleware.RequestDataIsProvided,
   PermissionMiddleware.UserLoggedIn,
   PermissionMiddleware.SecurityManagerLoggedIn,
   SecurityCompanyMiddleware.CreateGuardUserAccounts
+);
+
+// Create a security guard ~ contains extra details about the guards
+router.post(
+  "/security-guards",
+  UtilityMiddleware.RequestDataIsProvided,
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.SecurityManagerLoggedIn,
+  SecurityCompanyMiddleware.CreateSecurityGuardBatch
 );
 
 // Get security guards
@@ -43,6 +52,57 @@ router.delete(
 );
 
 //* SECURITY GUARD FUNCTIONALITY
+
+// Create resident checkin
+router.post(
+  "/guard/resident-checkin",
+  UtilityMiddleware.RequestDataIsProvided,
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.SecurityGuardLoggedIn,
+  SecurityCompanyMiddleware.CreateResidentCheckin
+);
+
+// Create visitor checkin
+router.post(
+  "/guard/visitor-checkin",
+  UtilityMiddleware.RequestDataIsProvided,
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.SecurityGuardLoggedIn,
+  PermissionMiddleware.VisitorCanCheckin,
+  SecurityCompanyMiddleware.CreateVisitorCheckin
+);
+
+// Get resident checkins
+router.get(
+  "/guard/resident-checkins",
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.SecurityGuardLoggedIn,
+  SecurityCompanyMiddleware.GetResidentCheckins
+);
+
+// Get visitor checkins
+router.get(
+  "/guard/visitor-checkins",
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.SecurityGuardLoggedIn,
+  SecurityCompanyMiddleware.GetVisitorCheckins
+);
+
+// Remove resident checkin
+router.delete(
+  "/guard/resident-checkin/:checkinId",
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.SecurityGuardLoggedIn,
+  SecurityCompanyMiddleware.RemoveResidentCheckin
+);
+
+// Remove visitor checkin
+router.delete(
+  "/guard/visitor-checkin/:checkinId",
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.SecurityGuardLoggedIn,
+  SecurityCompanyMiddleware.RemoveVisitorCheckin
+);
 
 //* EXPORTS
 module.exports = router;
