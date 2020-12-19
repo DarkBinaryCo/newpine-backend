e649856812ed3a13026df18e1030735ec42899df
AllanJeremy <aj.dev254@gmail.com>
2020-12-19 06:35:40 +0300

    [Enhancement] Added get user types & get identification types to utility middleware aggregator

--- src/middleware/UtilityMiddleware/index.js
+++ src/middleware/UtilityMiddleware/index.js
@@ -3,9 +3,13 @@
     UtilityMiddleware contains all middleware that could not fit in the other middleware categories.
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
+const GetIdentificationTypes = require("./GetIdentificationTypes");
+const GetUserTypes = require("./GetUserTypes");
 const RequestDataIsProvided = require("./RequestDataIsProvided");
 
 //* EXPORTS
 module.exports = {
+  GetIdentificationTypes,
+  GetUserTypes,
   RequestDataIsProvided,
 };
