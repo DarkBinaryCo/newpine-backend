a1821359649f745784f918745ac1ad6d7ab47ce9
AllanJeremy <aj.dev254@gmail.com>
2020-12-25 11:14:39 +0300

    [Feature] Added resident is verified middleware function to permission middleware

---
+++ src/middleware/PermissionMiddleware/ResidentIsVerified.js
@@ -0,0 +1,26 @@
+//* This middleware must be called after PermissionMiddleware.ResidentLoggedIn
+// Utils
+const { ApiUtil } = require("../../utils");
+
+/** Verify the currently logged in resident */
+const residentIsVerified = async (req, res, next) => {
+  if (!req.residentData) {
+    let responseMessage =
+      "It would appear you attempted to verify a resident without first confirming that the resident is logged in. Please call PermissionMiddleware.ResidentLoggedIn before attempting to verify resident.";
+    let apiResponse = ApiUtil.getError(responseMessage, {});
+
+    ApiUtil.printResponse(res, apiResponse, next);
+  } else if (req.residentData.isVerified === true) {
+    next();
+  } else {
+    //? Resident is not verified
+    let responseMessage =
+      "Permission denied! You need to be a verified resident to do that. Please request verification from your resident representative/chairperson.";
+    let apiResponse = ApiUtil.getUnauthorizedError(responseMessage);
+
+    ApiUtil.printResponse(res, apiResponse, next);
+  }
+};
+
+//* EXPORTS
+module.exports = residentIsVerified;
