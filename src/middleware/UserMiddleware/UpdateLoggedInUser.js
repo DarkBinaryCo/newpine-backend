f0c52cd9286e6261849dbecebb5cb74221e701de
AllanJeremy <aj.dev254@gmail.com>
2021-01-17 14:31:58 +0300

    [Feature] Added update  logged in user middleware function in user middleware

---
+++ src/middleware/UserMiddleware/UpdateLoggedInUser.js
@@ -0,0 +1,22 @@
+//* This middleware must be called after PermissionMiddleware.UserLoggedIn & UtilityMiddleware.RequestDataIsProvided
+// Services
+const { UserService } = require("../../services");
+
+// Utils
+const { ApiUtil } = require("../../utils");
+
+/** Update the logged in user with the data provided */
+const updateLoggedInUser = (req, res, next) => {
+  let filter = { id: req.userData.id };
+  let updateData = req.body.data;
+
+  //
+  ApiUtil.attachErrorHandler(
+    res,
+    UserService.updateUser(updateData, filter).then((_) => {
+      let apiResponse = ApiUtil.getResponse(true, "Successfully updated user");
+
+      ApiUtil.printResponse(res, apiResponse, next);
+    })
+  );
+};
