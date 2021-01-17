29728f46fddfea10b705eabe0b5d4da4cd0d2418
AllanJeremy <aj.dev254@gmail.com>
2021-01-17 14:31:42 +0300

    [Feature] Added get logged in user middleware function in user middleware

---
+++ src/middleware/UserMiddleware/GetLoggedInUser.js
@@ -0,0 +1,25 @@
+//* This middleware must be called after PermissionMiddleware.UserLoggedIn
+// Services
+const { UserService } = require("../../services");
+
+// Utils
+const { ApiUtil } = require("../../utils");
+
+/** Get the currently logged in user and return their data */
+const getLoggedInUser = (req, res, next) => {
+  //! Making a second get request to the user table to fetch additional information ~ possible area for optimization
+  let filter = { id: req.userData.id };
+
+  ApiUtil.attachErrorHandler(
+    res,
+    UserService.getSingleUser(filter).then((userFound) => {
+      let apiResponse = ApiUtil.getResponse(
+        true,
+        "Successfully retrieved user details",
+        userFound
+      );
+
+      ApiUtil.printResponse(res, apiResponse, next);
+    })
+  );
+};
