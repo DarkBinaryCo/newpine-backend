263012764128a0bbe7f27ee94fb6fcf42c95bb54
AllanJeremy <aj.dev254@gmail.com>
2021-02-02 17:40:36 +0300

    [Enhancement] Now converting phone into international format before sending OTP request

--- src/middleware/AuthMiddleware/SendOtp.js
+++ src/middleware/AuthMiddleware/SendOtp.js
@@ -2,18 +2,21 @@
 const { AuthService } = require("../../services");
 
 // Utils
-const { ApiUtil } = require("../../utils");
+const { ApiUtil, FormatUtil } = require("../../utils");
 
 /** Send an OTP the provided phone number */
 const sendOtp = (req, res, next) => {
   const { phone } = req.body.data;
 
+  // Convert the phone number to international so that our SMS provider API is not angry
+  const phoneInternational = FormatUtil.getPhoneInternationalFormat(phone);
+
   ApiUtil.attachErrorHandler(
     res,
-    AuthService.sendOtp(phone).then((response) => {
+    AuthService.sendOtp(phoneInternational).then((response) => {
       const isOk = response.hasSent && !response.isWaiting;
       let message = isOk
-        ? `Successfully sent OTP to ${phone}`
+        ? `Successfully sent OTP to ${phoneInternational}`
         : "Failed to send OTP";
       message = response.isWaiting
         ? "Failed: You need to wait before you can send another OTP request"
