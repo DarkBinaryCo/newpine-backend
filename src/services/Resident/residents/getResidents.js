38fc5acd427eb4cf6d2e20cf54a7cc2cd760f9e7
AllanJeremy <aj.dev254@gmail.com>
2021-03-31 09:48:06 +0300

    [Enhancement] Explicitly specified the user attributes that get returned when getting residents

--- src/services/Resident/residents/getResidents.js
+++ src/services/Resident/residents/getResidents.js
@@ -7,7 +7,22 @@ const { Resident, User } = require("../../../models");
 const getResidents = async (filter = {}, isAdmin = false) => {
   let findOptions = {
     where: filter,
-    include: { model: User, required: true },
+    include: {
+      model: User,
+      required: true,
+      attributes: [
+        "firstName",
+        "lastName",
+        "phone",
+        "email",
+        "userTypeId",
+        "userType",
+        "identificationTypeId",
+        "identificationNumber",
+        "isAdult",
+        "isNew",
+      ],
+    },
     paranoid: !isAdmin, //? Only admins can see all records (including paranoid deleted ones)
     // attributes: attributesToInclude,
   };
