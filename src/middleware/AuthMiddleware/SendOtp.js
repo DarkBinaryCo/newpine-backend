419b5b6d735885e0098571332d665c6c9120e50d
AllanJeremy <aj.dev254@gmail.com>
2021-04-21 14:42:37 +0300

    [Enhancement] Updated send OTP middleware to accept user type as part of the body data

--- src/middleware/AuthMiddleware/SendOtp.js
+++ src/middleware/AuthMiddleware/SendOtp.js
@@ -8,36 +8,40 @@ const { ApiUtil, FormatUtil } = require("../../utils");
 const sendOtp = (req, res, next) => {
   const { phone } = req.body.data;
 
+  const typeOfUserToCreate = req.body.data.userType;
+
   // Convert the phone number to international so that our SMS provider API is not angry
   //TODO: Add country code logic
   const phoneInternational = FormatUtil.getPhoneInternationalFormat(phone);
 
   ApiUtil.attachErrorHandler(
     res,
-    AuthService.sendOtp(phoneInternational).then((response) => {
-      const isOk = response.hasSent && !response.isWaiting;
-      let message = isOk
-        ? `Successfully sent OTP to ${phoneInternational}`
-        : "Failed to send OTP";
-      message = response.isWaiting
-        ? "Failed: You need to wait before you can send another OTP request"
-        : message;
-      const statusCode = isOk ? 200 : 403;
+    AuthService.sendOtp(phoneInternational, typeOfUserToCreate).then(
+      (response) => {
+        const isOk = response.hasSent && !response.isWaiting;
+        let message = isOk
+          ? `Successfully sent OTP to ${phoneInternational}`
+          : "Failed to send OTP";
+        message = response.isWaiting
+          ? "Failed: You need to wait before you can send another OTP request"
+          : message;
+        const statusCode = isOk ? 200 : 403;
 
-      const apiResponseData = {
-        phone,
-        ...response,
-      };
+        const apiResponseData = {
+          phone,
+          ...response,
+        };
 
-      const apiResponse = ApiUtil.getResponse(
-        isOk,
-        message,
-        apiResponseData,
-        statusCode
-      );
+        const apiResponse = ApiUtil.getResponse(
+          isOk,
+          message,
+          apiResponseData,
+          statusCode
+        );
 
-      ApiUtil.printResponse(res, apiResponse, next);
-    })
+        ApiUtil.printResponse(res, apiResponse, next);
+      }
+    )
   );
 };
 
