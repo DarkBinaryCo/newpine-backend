72f1de6f685c319bdeb3637ba26bfc74aaa92111
Allan Jeremy <aj.dev254@gmail.com>
2021-07-10 21:01:39 +0300

    [API] Added verify property residents resident representative route

--- src/routes/resident-rep.js
+++ src/routes/resident-rep.js
@@ -7,16 +7,28 @@ const {
   UtilityMiddleware,
 } = require("../middleware");
 
+// Get the resident owner for a given property
 router.get(
   "/resident/:propertyId",
   PermissionMiddleware.UserLoggedIn,
   PermissionMiddleware.ResidentRepLoggedIn
+  //TODO: Add implementation
 );
 
+// Verify residents belonging to a certain property
+router.post(
+  "/verify/residents/:propertyId",
+  PermissionMiddleware.UserLoggedIn,
+  PermissionMiddleware.ResidentRepLoggedIn,
+  ResidentMiddleware.VerifyPropertyResidents
+);
+
+// Get vehicles belonging to a given property
 router.get(
   "/vehicles/:propertyId",
   PermissionMiddleware.UserLoggedIn,
   PermissionMiddleware.ResidentRepLoggedIn
+  // TODO: Add implementation
 );
 
 //* EXPORTS
