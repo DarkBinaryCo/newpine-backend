0c0b7e9f38db84cf10df03e2717e466c4c10dd15
Allan Jeremy <aj.dev254@gmail.com>
2021-07-10 20:37:20 +0300

    [Enhancement] Added the ability to update resident verified status as a resident rep

--- src/services/Resident/residents/updateResident.js
+++ src/services/Resident/residents/updateResident.js
@@ -16,7 +16,7 @@ const updateResident = async (
 
   // Only resident admin should be able to update the active status
   if (isResidentRep) {
-    settableFields.push("isActive", "residentType");
+    settableFields.push("isActive", "isVerified", "residentType");
   }
 
   return Resident.update(updateData, { where: filter, fields: settableFields });
