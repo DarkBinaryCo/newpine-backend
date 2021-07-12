38d9a325912cf67155c989973438ebed76cc9014
Allan Jeremy <aj.dev254@gmail.com>
2021-07-12 08:57:46 +0300

    [Enhancement] Added the ability to include or exclude resident details when fetching vehicles

--- src/services/Resident/vehicles/getVehicles.js
+++ src/services/Resident/vehicles/getVehicles.js
@@ -2,13 +2,18 @@ const { Resident, Vehicle } = require("../../../models");
 
 /** Get vehicles
  * @param {Object} filter Vehicle query filter
+ * @param {Boolean} includeResident Whether we should include the resident details or not
  */
-const getVehicles = async (filter = {}) => {
+const getVehicles = async (filter = {}, includeResident = true) => {
   let findOptions = {
     where: filter,
-    include: { model: Resident, required: true },
     order: [["updatedAt", "DESC"]],
   };
+
+  // Only include the resident details if we have asked for them
+  if (includeResident === true) {
+    findOptions.include = { model: Resident, required: true };
+  }
 
   return Vehicle.findAll(findOptions);
 };
