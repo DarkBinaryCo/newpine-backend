7c60314e811f5e1601e2777c6a94b1a84707567d
Allan Jeremy <aj.dev254@gmail.com>
2021-07-13 17:21:00 +0300

    [Enhancement] Added option for choosing whether to return user/property data when getting a single resident This can save us fetching unnecessary data in some cases

--- src/services/Resident/residents/getSingleResident.js
+++ src/services/Resident/residents/getSingleResident.js
@@ -4,19 +4,41 @@ const {
   PropertyGroup,
   PropertyGroupType,
   PropertyType,
+  User,
 } = require("../../../models");
 
 /** Get a single resident by a filter
  * @param {String} filter Get filter
  * @param {Boolean} isAdmin Whether the caller of the function is an admin or not. Admins can view 'deleted' resident accounts.
+ * @param {Boolean} includeUser Whether to include the user details as part of the resident data
+ * @param {Boolean} includeProperty Whether to include the property details as part of the resident data
  * @return {Object|null} An object containing the resident found(if any) or null if no resident by the `userId` was found
  */
-const getSingleResident = async (filter, isAdmin = false) => {
+const getSingleResident = async (
+  filter,
+  isAdmin = false,
+  includeUser = false,
+  includeProperty = true
+) => {
   //TODO: Set the attributes that should be returned
-  let residentFound = await Resident.findOne({
+  const findOptions = {
     where: filter,
     paranoid: !isAdmin, //? Only admins can see all records (including paranoid deleted ones)
-    include: {
+    include: [],
+  };
+
+  // Include user data
+  if (includeUser === true) {
+    findOptions.include.push({
+      model: User,
+      required: true,
+      attributes: ["id", "firstName", "lastName", "gender", "dob"],
+    });
+  }
+
+  // Include property data
+  if (includeProperty === true) {
+    findOptions.include.push({
       model: Property,
       required: true,
       attributes: ["id", "propertyGroupId", "propertyNumber", "propertyTypeId"],
@@ -37,9 +59,13 @@ const getSingleResident = async (filter, isAdmin = false) => {
           attributes: ["id", "name"],
         },
       ],
-    },
-  });
+    });
+  }
 
+  // Actually find the resident
+  let residentFound = await Resident.findOne(findOptions);
+
+  //
   const residentData = residentFound ? residentFound.dataValues : null;
 
   return residentData;
