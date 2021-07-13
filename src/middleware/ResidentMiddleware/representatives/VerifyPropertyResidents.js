3d8ec28d3058af7a961fa7afa25acd04124b934f
Allan Jeremy <aj.dev254@gmail.com>
2021-07-13 18:48:01 +0300

    [Feature] Added the ability to unverify property residents

--- src/middleware/ResidentMiddleware/representatives/VerifyPropertyResidents.js
+++ src/middleware/ResidentMiddleware/representatives/VerifyPropertyResidents.js
@@ -8,32 +8,36 @@ const { ApiUtil } = require("../../../utils");
 /** Verify a resident account (and user account belonging to the resident) with a given user ID - used by resident representatives */
 const verifyResidentAccount = async (req, res, next) => {
   const { propertyId } = req.params;
+  const isVerify = req.body.data.verify || false;
 
   ApiUtil.attachErrorHandler(
     res,
-    ResidentService.updatePropertyResidentVerification(propertyId, true).then(
-      (response) => {
-        let apiResponse;
+    ResidentService.updatePropertyResidentVerification(
+      propertyId,
+      isVerify
+    ).then((response) => {
+      let apiResponse;
 
-        //! This relies on data being returned when there is no residents being exactly `false`. Potential area for bugs
-        if (response === false) {
-          apiResponse = ApiUtil.getError(
-            "[Error] That property doesn't see to have any residents as of now",
-            null,
-            404
-          );
-          return ApiUtil.printResponse(res, apiResponse);
-        } else {
-          //
-          apiResponse = ApiUtil.getResponse(
-            true,
-            `Successfully verified the existing property residents`
-          );
-        }
+      //! This relies on data being returned when there is no residents being exactly `false`. Potential area for bugs
+      if (response === false) {
+        apiResponse = ApiUtil.getError(
+          "[Error] That property doesn't see to have any residents as of now",
+          null,
+          404
+        );
+        return ApiUtil.printResponse(res, apiResponse);
+      } else {
+        const verb = isVerify ? "verified" : "unverified";
 
-        ApiUtil.printResponse(res, apiResponse);
+        //
+        apiResponse = ApiUtil.getResponse(
+          true,
+          `Successfully ${verb} the property residents`
+        );
       }
-    )
+
+      ApiUtil.printResponse(res, apiResponse);
+    })
   );
 };
 
