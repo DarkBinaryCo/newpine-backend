6b984154b8876849b6919c0879b4ce5e5159da14
Allan Jeremy <aj.dev254@gmail.com>
2021-07-13 18:45:16 +0300

    [Refactoring] Updated argument name for updating the property resident to match other areas of the codebase

--- src/services/Resident/residentReps/updatePropertyResidentVerification.js
+++ src/services/Resident/residentReps/updatePropertyResidentVerification.js
@@ -6,11 +6,11 @@ const { getResidents, updateResident } = require("../residents");
 
 /** Update the verification status of residents belonging to a certain property
  * @param {Int} propertyId The id of the property whose residents accounts we want to activate
- * @param {Boolean} isVerified Whether to activate or deactivate these accounts. Defaults to `true` - 'activate'
+ * @param {Boolean} isVerify Whether to activate or deactivate these accounts. Defaults to `true` - 'activate'
  */
 const updatePropertyResidentVerification = async (
   propertyId,
-  isVerified = true
+  isVerify = true
 ) => {
   // We'll use this resident list to individually update users
   const residentsToUpdate = await getResidents({ propertyId });
@@ -20,14 +20,20 @@ const updatePropertyResidentVerification = async (
 
   // Update the resident accounts
   //! Potential performance bottleneck ~ O(n+2) operations to verify residents where `n` is the number of residents in a property
-  return updateResident({ isVerified }, { propertyId }, true).then(() => {
-    // Update the individual users belonging to the resident accounts
-    residentsToUpdate.forEach(async (resident) => {
-      const userUpdateFilter = { id: resident.userId };
+  return updateResident({ isVerified: isVerify }, { propertyId }, true).then(
+    () => {
+      // Update the individual users belonging to the resident accounts
+      residentsToUpdate.forEach(async (resident) => {
+        const userUpdateFilter = { id: resident.userId };
 
-      await UserService.updateUser({ isVerified }, userUpdateFilter, true);
-    });
-  });
+        await UserService.updateUser(
+          { isVerified: isVerify },
+          userUpdateFilter,
+          true
+        );
+      });
+    }
+  );
 };
 
 //* EXPORTS
