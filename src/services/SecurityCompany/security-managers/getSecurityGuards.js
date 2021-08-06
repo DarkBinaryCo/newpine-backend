904cf51a46bdfba9bb3bc085240c85d6b7a612f2
Allan Jeremy <aj.dev254@gmail.com>
2021-08-06 15:27:20 +0300

    [Enhancement] Limited security guard fields being returned & added property group details to returned data

--- src/services/SecurityCompany/security-managers/getSecurityGuards.js
+++ src/services/SecurityCompany/security-managers/getSecurityGuards.js
@@ -1,9 +1,32 @@
-const { SecurityGuard, SecurityShift, User } = require("../../../models");
+const {
+  PropertyGroup,
+  PropertyGroupType,
+  SecurityGuard,
+  SecurityShift,
+  User,
+} = require("../../../models");
 
 /** Get security guard
  * @param {Object} filter Security guard query filter
  */
 const getSecurityGuards = async (filter = {}) => {
+  // Security guard attributes to fetch
+  const securityGuardFieldsToFetch = [
+    "id",
+    "propertyGroupId",
+    "shiftId",
+    "userId",
+  ];
+
+  // Property group related
+  const propertyGroupFieldsToFetch = ["name", "phase"];
+  const propertyGroupTypeFieldsToFetch = [
+    "name",
+    "friendlyName",
+    "isIndependentCluster",
+    "canHaveResidentAccount",
+  ];
+
   //
   const securityShiftFieldsToFetch = ["name", "startTime", "endTime"];
 
@@ -20,14 +43,23 @@ const getSecurityGuards = async (filter = {}) => {
   let findOptions = {
     where: filter,
     include: [
-      { model: User, required: true, attributes: userFieldsToFetch },
+      {
+        model: PropertyGroup,
+        required: true,
+        attributes: propertyGroupFieldsToFetch,
+        include: {
+          model: PropertyGroupType,
+          attributes: propertyGroupTypeFieldsToFetch,
+        },
+      },
       {
         model: SecurityShift,
         required: true,
         attributes: securityShiftFieldsToFetch,
       },
+      { model: User, required: true, attributes: userFieldsToFetch },
     ],
-    // attributes: attributesToInclude,
+    attributes: securityGuardFieldsToFetch,
   };
 
   return SecurityGuard.findAll(findOptions);
