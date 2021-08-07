078048d6b3d9597841cbd7f88de0054d963e318f
Allan Jeremy <aj.dev254@gmail.com>
2021-08-07 08:03:35 +0300

    [Enhancement] Get property groups now returns some information about the group th e property belongs to

--- src/services/Property/getPropertyGroups.js
+++ src/services/Property/getPropertyGroups.js
@@ -1,12 +1,18 @@
 // Models
-const { PropertyGroup } = require("../../models");
+const { PropertyGroup, PropertyGroupType } = require("../../models");
 
 /** Get all property groups matching a filter
  * @param {Object} filter Query filter. Defaults to `{}` thus fetching all property groups
  * @return {Array<Object>} An array containing a list of property groups found matching the given filter
  */
 const getPropertyGroups = (filter = {}) => {
-  return PropertyGroup.findAll({ where: filter });
+  return PropertyGroup.findAll({
+    where: filter,
+    include: {
+      model: PropertyGroupType,
+      attributes: ["name", "isIndependentCluster"],
+    },
+  });
 };
 
 //* EXPORTS
