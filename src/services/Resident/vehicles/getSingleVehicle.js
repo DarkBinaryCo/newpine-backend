b09d2571f71e46213c0da8ad75cd853d9f29da2b
Allan Jeremy <aj.dev254@gmail.com>
2021-08-20 11:06:16 +0300

    [Feature] Added function for getting a single vehicle (resident 🚗 to be specific)

---
+++ src/services/Resident/vehicles/getSingleVehicle.js
@@ -0,0 +1,26 @@
+// Models
+const { Vehicle } = require("../../models");
+
+/** Get a single vehicle that matches the filter provided
+ * @param {Object} filter The sequelize filter the vehicle has to match to be returned
+ * @param {Array<String>} extraAttributesToInclude An array of attributes to include along with the default included attributes
+ * @return {Object|null} Return an object representing the vehicle's data if found or null if no vehicle is found
+ */
+const getSingleVehicle = async (filter, extraAttributesToInclude = []) => {
+  //
+  let attributesToInclude = ["id", "name", "model", "numberplate"];
+
+  // Add extra attributes
+  attributesToInclude = attributesToInclude.concat(extraAttributesToInclude);
+
+  const vehicleFound = await Vehicle.findOne({
+    where: filter,
+    attributes: attributesToInclude,
+  });
+
+  const vehicleData = vehicleFound ? vehicleFound.dataValues : null;
+  return vehicleData;
+};
+
+//* EXPORTS
+module.exports = getSingleVehicle;
--- src/services/Resident/vehicles/index.js
+++ src/services/Resident/vehicles/index.js
@@ -3,6 +3,7 @@
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 const addVehicle = require("./addVehicle");
+const getSingleVehicle = require("./getSingleVehicle");
 const getVehicles = require("./getVehicles");
 const updateVehicle = require("./updateVehicle");
 const removeVehicle = require("./removeVehicle");
@@ -10,6 +11,7 @@ const removeVehicle = require("./removeVehicle");
 //* EXPORTS
 module.exports = {
   addVehicle,
+  getSingleVehicle,
   getVehicles,
   updateVehicle,
   removeVehicle,
