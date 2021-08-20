1a8964cc2a6e39184a117a146bed3a8dfed8ae1a
Allan Jeremy <aj.dev254@gmail.com>
2021-08-20 16:59:40 +0300

    [Enhancement] Updated create visitor checkin function parameters Updated function definition to account for new params as well as setting of visitorInvitationId & securityGuardId

--- src/services/SecurityCompany/security-guards/createVisitorCheckin.js
+++ src/services/SecurityCompany/security-guards/createVisitorCheckin.js
@@ -5,18 +5,59 @@ const { VisitorCheckin } = require("../../../models");
 const VisitorService = require("../../../services/Visitor");
 
 /** Create a resident checkin
- * @param {Object} insertData Data to be inserted into the database
+ * @param {UUID} visitorInvitationId The id of the visitor invitation
+ * @param {UUID} securityGuardId The id of the security guard checking in the resident
+ * @param {boolean} isCheckin Is this a checkin(`true`) or a checkout(`false`)
+ * @param {{propertyId: number}} insertData Data to be inserted into the database
  * @return {Promise<Object>} A promise that resolves to an object with the create operation information
  */
-const createVisitorCheckin = async (insertData) => {
-  let settableFields = ["visitorInvitationId", "securityGuardId", "isCheckin"];
+const createVisitorCheckin = async (
+  visitorInvitationId,
+  securityGuardId,
+  isCheckin,
+  insertData = {}
+) => {
+  let settableFields = [
+    "propertyId",
+    "securityGuardId",
+    "visitorInvitationId",
+    "isCheckin",
+  ];
 
+  // Visitor invitation
+  const invitationFound = await VisitorService.getSingleVisitorInvitation({
+    id: visitorInvitationId,
+  });
+
+  // No point going on if there is no visitor invitation to map to the checkin
+  if (!invitationFound) {
+    throw new Error(
+      "Could not find a visitor invitation with that ID. Please try again with a valid visitor invitation id"
+    );
+  }
+
+  //* Getting here means there was a visitor invitation found that had the provided visitor invitation id
+  // When we attempt to checkin a visitor with an invitation that has already been used, don't allow them to  checkin
+  if (!invitationFound.isActive && isCheckin) {
+    throw new Error(
+      "Checkin failed. That invitation id has already been used and cannot be re-used."
+    );
+  }
+
+  //* Getting here means the visitor invitation has not yet been used or the user is checking out
   // De-activate the visitor invitation as soon as they have been checked-in
-  let updateFilter = { id: insertData.visitorInvitationId };
-  let updateData = { isActive: false };
+  let updateFilter = { id: visitorInvitationId };
 
-  //? De-activating means the visitor can be checked out but they cannot use the same invitation to get in
-  VisitorService.updateVisitorInvitation(updateData, updateFilter);
+  // De-activating means the visitor can be checked out but they cannot use the same invitation to get in
+  VisitorService.updateVisitorInvitation(updateFilter, { isActive: false });
+
+  // Explicitly add the and security guard id data to the `insertData` object
+  insertData = {
+    ...insertData,
+    isCheckin,
+    securityGuardId,
+    visitorInvitationId,
+  };
 
   return VisitorCheckin.create(insertData, {
     fields: settableFields,
