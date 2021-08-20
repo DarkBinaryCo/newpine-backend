6b9328f362168bb0277ccba157a9d9bd29146e39
Allan Jeremy <aj.dev254@gmail.com>
2021-08-20 16:27:43 +0300

    [Enhancement] Updated create resident checkin function parameters Updated function definition to account for new params as well as setting of residentId

--- src/services/SecurityCompany/security-guards/createResidentCheckin.js
+++ src/services/SecurityCompany/security-guards/createResidentCheckin.js
@@ -1,11 +1,27 @@
 const { ResidentCheckin } = require("../../../models");
 
 /** Create a resident checkin
- * @param {Object} insertData Data to be inserted into the database
+ * @param {UUID} residentId The id of the resident being checked in
+ * @param {UUID} securityGuardId The id of the security guard checking in the resident
+ * @param {boolean} isCheckin Is this a checkin(`true`) or a checkout(`false`)
+ * @param {{vehicleId: string}} insertData Data to be inserted into the database. Default `{}`
  * @return {Promise<Object>} A promise that resolves to an object with the create operation information
  */
-const createResidentCheckin = async (insertData) => {
-  let settableFields = ["vehicleId", "securityGuardId", "isCheckin"];
+const createResidentCheckin = async (
+  residentId,
+  securityGuardId,
+  isCheckin,
+  insertData = {}
+) => {
+  let settableFields = [
+    "residentId",
+    "securityGuardId",
+    "vehicleId",
+    "isCheckin",
+  ];
+
+  // Explicitly add the resident id and security id data to the `insertData` object
+  insertData = { ...insertData, isCheckin, residentId, securityGuardId };
 
   return ResidentCheckin.create(insertData, {
     fields: settableFields,
