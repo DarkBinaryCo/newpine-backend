dd970e5fa3d715a09328dfba8ae1241695ecc448
Allan Jeremy <aj.dev254@gmail.com>
2021-08-24 10:21:41 +0300

    [Bugfix] Fixed issue with security guard only user filter

--- src/filters/UserFilter/SecurityGuardOnly.js
+++ src/filters/UserFilter/SecurityGuardOnly.js
@@ -2,7 +2,10 @@ const { USER_TYPE } = require("../../config/auth");
 
 /** Set filter that states we should only affect security guard user accounts */
 const securityGuardOnly = (req, res, next) => {
-  req.body.filter = { ...req.body.filter, id: USER_TYPE.SECURITY_GUARD };
+  req.body.filter = {
+    ...req.body.filter,
+    userTypeId: USER_TYPE.SECURITY_GUARD,
+  };
   next();
 };
 
