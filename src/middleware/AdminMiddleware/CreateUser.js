501d1304f6139dcc61bcbed637e205efa1369fbb
Allan Jeremy <aj.dev254@gmail.com>
2021-08-24 17:13:42 +0300

    [Feature] Added create user middleware function as part of admin middleware

---
+++ src/middleware/AdminMiddleware/CreateUser.js
@@ -0,0 +1,50 @@
+// Config
+const { USER_TYPE } = require("../../config/auth");
+
+// Services
+const { UserService } = require("../../services");
+
+// Utils
+const { ApiUtil } = require("../../utils");
+
+/**  Add a security manager account. This is simply a user account with a userType of Security manager */
+const createUser = (req, res, _) => {
+  let userData = req.body.data || {};
+  const { userTypeId } = req.params;
+
+  const acceptableUserTypes = [
+    USER_TYPE.ADMIN,
+    USER_TYPE.SALES,
+    USER_TYPE.SECURITY_MANAGER,
+  ];
+
+  // No point proceeding if we are trying to create a user type we should not be able to create as an admin
+  if (!acceptableUserTypes.includes(userTypeId)) {
+    const error = new Error("Unable to create a user of that type");
+    const apiResponse = ApiUtil.getError(error.message, error, 400);
+
+    ApiUtil.printResponse(res, apiResponse);
+  }
+
+  //* Getting here means we are allowed to create the user type
+  userData.userTypeId = userTypeId;
+  userData.isActive = true;
+
+  //
+  ApiUtil.attachErrorHandler(
+    res,
+    UserService.createUser(userData, true).then((_) => {
+      let apiResponse = ApiUtil.getResponse(
+        true,
+        "Successfully created security manager account",
+        null,
+        201
+      );
+
+      ApiUtil.printResponse(res, apiResponse);
+    })
+  );
+};
+
+//* EXPORTS
+module.exports = createUser;
--- src/middleware/AdminMiddleware/index.js
+++ src/middleware/AdminMiddleware/index.js
@@ -2,9 +2,9 @@
     This file is simply meant to be an aggregator of all AdminMiddleware related functions.
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
-const AddSecurityManager = require("./AddSecurityManager");
+const CreateUser = require("./CreateUser");
 
 //* EXPORTS
 module.exports = {
-  AddSecurityManager,
+  CreateUser,
 };
