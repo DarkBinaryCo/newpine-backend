deb2587ad298dc684add11d1c22f74cef761b0b8
Allan Jeremy <aj.dev254@gmail.com>
2021-08-25 07:05:58 +0300

    [Enhancement] Fetching user type name when getting users. This will allow us to tell what kind of users we are dealing with

--- src/services/User/getUsers.js
+++ src/services/User/getUsers.js
@@ -1,12 +1,19 @@
 // Models
-const { User } = require("../../models");
+const { User, UserType } = require("../../models");
 
 /** Get all users matching a filter
  * @param {Object} filter Query filter. Defaults to `{}` thus fetching all user types
  * @return {Array<Object>} An array containing a list of user types found matching the given filter
  */
 const getUsers = (filter = {}) => {
-  return User.findAll({ where: filter, order: [["updatedAt", "DESC"]] });
+  return User.findAll({
+    where: filter,
+    include: {
+      model: UserType,
+      attributes: ["name"],
+    },
+    order: [["updatedAt", "DESC"]],
+  });
 };
 
 //* EXPORTS
