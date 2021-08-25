3f88b8b0c82f7a9da5bedf6d19089f9c9fa5c75a
Allan Jeremy <aj.dev254@gmail.com>
2021-08-25 10:14:12 +0300

    [Bugfix] Fixed lodash reference issue in get users middleware function

--- src/middleware/AdminMiddleware/GetUsers.js
+++ src/middleware/AdminMiddleware/GetUsers.js
@@ -9,7 +9,7 @@ const { UserService } = require("../../services");
 const { ApiUtil } = require("../../utils");
 
 /**  Add a security manager account. This is simply a user account with a userType of Security manager */
-const getUsers = (req, res, _) => {
+const getUsers = (req, res, _next) => {
   const filter = {
     id: {
       [Op.not]: req.userData.id, // Exclude the currently logged in user
