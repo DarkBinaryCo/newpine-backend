fa634178b0800740c177ddec2eeba1a95c8993fb
Allan Jeremy <aj.dev254@gmail.com>
2021-08-27 12:15:44 +0300

    [Bugfix] Fixed issue with admin create user middleware function not allowing creation of any account type Also added resident rep as one of the account types that an admin can create an account for

--- src/middleware/AdminMiddleware/CreateUser.js
+++ src/middleware/AdminMiddleware/CreateUser.js
@@ -15,11 +15,12 @@ const createUser = (req, res, _) => {
   const acceptableUserTypes = [
     USER_TYPE.ADMIN,
     USER_TYPE.SALES,
+    USER_TYPE.RESIDENT_REP,
     USER_TYPE.SECURITY_MANAGER,
   ];
 
   // No point proceeding if we are trying to create a user type we should not be able to create as an admin
-  if (!acceptableUserTypes.includes(userTypeId)) {
+  if (!acceptableUserTypes.includes(parseInt(userTypeId))) {
     const error = new Error("Unable to create a user of that type");
     const apiResponse = ApiUtil.getError(error.message, error, 400);
 
@@ -27,7 +28,7 @@ const createUser = (req, res, _) => {
   }
 
   //* Getting here means we are allowed to create the user type
-  userData.userTypeId = userTypeId;
+  userData.userTypeId = parseInt(userTypeId);
   userData.isActive = true;
 
   //
