67e310ba15563beacab4dc64b6dfc66a2cbf3f70
Allan Jeremy <aj.dev254@gmail.com>
2021-08-28 13:32:47 +0300

    [Enhancement] Made error handler in api util slightly more advanced Since most sequelize errors leak information we don't want our users to know, we only return the errors found (without any database specific details) as an array when something happens. This prevents revealing our database table structure when making API requests that trigger errors in the database
    Also, this is likely a 🔥 abstraction because pretty much every API request will do something with the database, which implies that if something were to go wrong, this would probably be the most elegant way to solve it.
    We could also test debug for the actual error details on a dev machine
    
    The major downside is that it is going to need updating to actually fetch friendly messages in other ORMs

--- src/utils/api/attachErrorHandler.js
+++ src/utils/api/attachErrorHandler.js
@@ -1,19 +1,29 @@
 // Internal
-const getError = require('./getError');
+const getError = require("./getError");
 
-/** Attaches an error handler to an `async` function 
+/** Attaches an error handler to an `async` function
  * @param {Object} res A HTTP resource object. Usually the first parameter of an Express middleware function
  * @param {Function} fn The `async` function to attach the error message to
  * @return A promise with an error handler attached to it
  */
-const attachErrorHandler = (res, fn) => { //? Intended to be called in middleware
-    return fn.catch(err => {
-        let apiResponse = getError(err.message, err);
-        console.log(err);
-        console.error(err.message);
+const attachErrorHandler = (res, fn) => {
+  //? Intended to be called in middleware
+  return fn.catch((err) => {
+    let apiResponse;
 
-        res.status(500).json(apiResponse);
-    });
+    // When we have the equivalent of sequelize errors (which contain an array of 'errors')
+    //! Update this logic to fetch user friendly messages if we decide to use a different library from sequelize to access the db
+    if (err.errors) {
+      apiResponse = getError(err.message, err.errors);
+    } else {
+      apiResponse = getError(err.message, err);
+    }
+
+    console.log(err);
+    console.error(err.message);
+
+    res.status(500).json(apiResponse);
+  });
 };
 
 //* EXPORTS
