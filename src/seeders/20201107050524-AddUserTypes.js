af851759e7cca5908e8edd88661caed2bc10d396
Allan Jeremy <aj.dev254@gmail.com>
2021-08-28 12:52:47 +0300

    [Database] Updated add users seeder to account for the new user type table columns

--- src/seeders/20201107050524-AddUserTypes.js
+++ src/seeders/20201107050524-AddUserTypes.js
@@ -7,34 +7,41 @@ const DEFAULT_USER_TYPES = [
   {
     id: 1,
     name: "admin",
+    friendlyName: "Administrator",
     description: "Administrator - ideally Dark Binary.",
   },
   {
     id: 2,
     name: "sales",
+    friendlyName: "Sales",
     description:
       "Sales account - used for sending out messages to users of the system",
   },
   {
     id: 3,
     name: "security_manager",
+    friendlyName: "Security manager",
     description:
       "Security management representative - can checkin security and add guards",
   },
   {
     id: 4,
     name: "security_guard",
-    description: "Security guard",
+    friendlyName: "Security guard",
+    description:
+      "Security guard. Can checkin/checkout residents, visitors and stuff",
   },
   {
     id: 5,
     name: "resident_rep",
+    friendlyName: "Resident representative",
     description:
       "Resident representative - can activate resident accounts as well as resident vehicles",
   },
   {
     id: 6,
     name: "resident",
+    friendlyName: "Resident",
     description: "Resident. Includes co-residents",
   },
 ];
