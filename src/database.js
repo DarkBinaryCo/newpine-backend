1063ce2780cd8a44c3908bba13b09badcc057541
Allan Jeremy <aj.dev254@gmail.com>
2021-08-29 11:18:00 +0300

    added ssl mode required param to db connection string

--- src/database.js
+++ src/database.js
@@ -8,7 +8,7 @@ let sequelize;
 // Local versions will use environment settings while production versions will use connection string ("more secure")
 switch (process.env.ENVIRONMENT) {
   case "production":
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
+    sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
       dialect: "postgres",
       protocol: "postgres",
       dialectOptions: {
