a2360ee2743ef510c718823203b010508c6fed11
Allan Jeremy <aj.dev254@gmail.com>
2021-08-29 07:49:53 +0300

    [Enhancement] Updated database connection to use connection string in production Local connection behaviour has not changed

--- src/database.js
+++ src/database.js
@@ -2,17 +2,27 @@ const Sequelize = require("sequelize");
 const DbConfig = require("./config/database");
 
 //* Database setup
-const sequelize = new Sequelize(
-  DbConfig.database,
-  DbConfig.username,
-  DbConfig.password,
-  {
-    dialect: DbConfig.dialect,
+let sequelize;
 
-    //? Show SQL logs in the console (useful for debugging)
-    logging: false,
-  }
-);
+// Local versions will use environment settings while production versions will use connection string ("more secure")
+switch (process.env.ENVIRONMENT) {
+  case "production":
+    sequelize = new Sequelize(process.env.DATABASE_URL);
+    break;
+
+  default:
+    sequelize = new Sequelize(
+      DbConfig.database,
+      DbConfig.username,
+      DbConfig.password,
+      {
+        dialect: DbConfig.dialect,
+
+        //? Show SQL logs in the console (useful for debugging)
+        logging: false,
+      }
+    );
+}
 
 //* EXPORTS
 module.exports = sequelize;
