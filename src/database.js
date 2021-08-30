65a675ccdf5b852ca0ed183c9bfad8d8545fe616
Allan Jeremy <aj.dev254@gmail.com>
2021-08-30 11:30:27 +0300

    [Cleanup] Removed unnecessary console log in database connection file

--- src/database.js
+++ src/database.js
@@ -5,8 +5,6 @@ const Sequelize = require("sequelize");
 //* Database setup
 let sequelize;
 
-console.debug("Environment: ", process.env.ENVIRONMENT);
-
 //? When the database url is set it means we are on the server and have received a connection string as DATABASE_URL (typically heroku-postgres)
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
