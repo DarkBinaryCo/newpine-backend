25655ad953ad5e718837d20723f696565aced924
Allan Jeremy <aj.dev254@gmail.com>
2021-08-30 11:28:27 +0300

    [Bugfix] Fixed issue with connection to the database not working in deployment

--- src/database.js
+++ src/database.js
@@ -7,36 +7,32 @@ let sequelize;
 
 console.debug("Environment: ", process.env.ENVIRONMENT);
 
-// Local versions will use environment settings while production versions will use connection string ("more secure")
-switch (process.env.ENVIRONMENT) {
-  case "production":
-    console.debug("Database url: ", process.env.DATABASE_URL, "\n");
-
-    sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
-      dialect: "postgres",
-      protocol: "postgres",
-      native: true,
-      host: process.env.DB_HOST,
-      dialectOptions: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
+//? When the database url is set it means we are on the server and have received a connection string as DATABASE_URL (typically heroku-postgres)
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
+    dialect: "postgres",
+    protocol: "postgres",
+    host: process.env.DB_HOST,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
       },
-    });
-    break;
+    },
+  });
+} else {
+  //? This means we are building locally
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      dialect: process.env.DB_DIALECT,
 
-  default:
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USERNAME,
-      process.env.DB_PASSWORD,
-      {
-        dialect: process.env.DB_DIALECT,
-
-        //? Show SQL logs in the console (useful for debugging)
-        logging: false,
-      }
-    );
+      //? Show SQL logs in the console (useful for debugging)
+      logging: false,
+    }
+  );
 }
 
 //* EXPORTS
