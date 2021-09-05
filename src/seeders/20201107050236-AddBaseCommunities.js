63e0e8205a2aee5cf4e42f997af7c319cbf45deb
Allan Jeremy <aj.dev254@gmail.com>
2021-09-05 10:30:44 +0300

    [Database] Added base communities seeder TODO: Add relationships between communities and the rest of the system

---
+++ src/seeders/20201107050236-AddBaseCommunities.js
@@ -0,0 +1,33 @@
+"use strict";
+// Helpers
+const setDefaultDates = require("./helpers/setDefaultDates");
+const setUUIDIds = require("./helpers/setUUIDIds");
+
+const TABLE_NAME = "communities";
+
+const BASE_COMMUNITIES = [
+  {
+    name: "Nyayo Estate",
+    description:
+      "The largest and cleanest estate in East Africa built by NSSF in the 90s",
+    location: "Nyayo Estate Embakasi, Nairobi",
+    latitude: -1.3365,
+    longitude: 36.9244,
+  },
+];
+
+//* EXPORTS
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete(TABLE_NAME);
+
+    queryInterface.bulkInsert(
+      TABLE_NAME,
+      setDefaultDates(setUUIDIds(BASE_COMMUNITIES), Sequelize)
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete(TABLE_NAME);
+  },
+};
