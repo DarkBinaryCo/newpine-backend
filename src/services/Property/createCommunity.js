63cc8389af35c71d99b65468078dcfe1a3c8a52e
Allan Jeremy <aj.dev254@gmail.com>
2021-09-12 09:55:46 +0300

    [Feature] Added community related functionality to property service

---
+++ src/services/Property/createCommunity.js
@@ -0,0 +1,26 @@
+// Models
+const { Community } = require("../../models");
+
+/** Create a new community with `insertData`
+ * @param {Object} insertData Data to enter into the database
+ * @param {Boolean} isAdmin Admins can verify/unverify new accounts
+ * @return {Promise<Object>} A promise that resolves to an object with the create operation information
+ */
+const createCommunity = async (insertData = {}) => {
+  let settableFields = [
+    "name",
+    "type",
+    "description",
+    "location",
+    "latitude",
+    "longitude",
+  ];
+
+  return Community.create(insertData, {
+    fields: settableFields,
+    returning: true,
+  });
+};
+
+//* EXPORTS
+module.exports = createCommunity;
