6f5667097b4cc6319929094006140234e57b878a
Allan Jeremy <aj.dev254@gmail.com>
2021-09-28 18:03:59 +0300

    [Refactoring] Moved create new user functionality out of sendOtp service function Moved the functionality into the middleware that actually does the sending of the OTP

--- src/middleware/AuthMiddleware/SendOtp.js
+++ src/middleware/AuthMiddleware/SendOtp.js
@@ -1,8 +1,9 @@
 // Config
 const { USER_TYPE } = require("../../config/auth");
+const { DEFAULT_COMMUNITY_ID } = require("../../seeders/config");
 
 // Services
-const { AuthService, UserService } = require("../../services");
+const { AuthService, UserService, PropertyService } = require("../../services");
 
 // Utils
 const { ApiUtil, FormatUtil } = require("../../utils");
@@ -12,58 +13,73 @@ const sendOtp = async (req, res, next) => {
   const { phone } = req.body.data;
 
   // Convert the phone number to international so that our SMS provider API is not angry
-  //TODO: Add country code logic
   const phoneInternational = FormatUtil.getPhoneInternationalFormat(phone);
 
-  // Only allow certain user types a user can create (only resident and resident rep)
   const userTypesAllowed = [USER_TYPE.RESIDENT, USER_TYPE.RESIDENT_REP];
   const typeOfUserToCreate = req.body.data.userType;
 
   // Check if the user already exists -> Allow them to signin if so
-  const userExists = await UserService.getSingleUser({
-    phone: phoneInternational,
-  });
+  let user = await UserService.getSingleUser({ phone: phoneInternational }, [
+    "otp",
+    "lastOtpSentAt",
+  ]);
 
+  // When no user type is specified, allow them to signup ~ defaults to resident
   const isValidUserType = typeOfUserToCreate
     ? userTypesAllowed.includes(parseInt(typeOfUserToCreate))
     : true;
 
-  // Don't allow creation of any other type of user that isn't explicitly allowed here and doesn't already have an account
-  if (!userExists && !isValidUserType) {
-    const error = new Error("Signup is not available for that account type");
+  //
+  try {
+    if (!user) {
+      // Don't allow creation of any other type of user that isn't explicitly allowed here and doesn't already have an account
+      if (!isValidUserType) {
+        throw new Error("Signup is not available for that account type");
+      }
 
-    const apiResponse = ApiUtil.getError(error.message, error, 400);
+      const newUserData = { phone };
+
+      //
+      if (typeOfUserToCreate) {
+        newUserData.userTypeId = typeOfUserToCreate;
+      }
+
+      // If the user does not exist ~ create them
+      const newUser = await UserService.createUser(newUserData);
+      user = newUser.dataValues;
+    }
+  } catch (err) {
+    const apiResponse = ApiUtil.getError(err.message, err);
     return ApiUtil.printResponse(res, apiResponse);
   }
 
+  //
   ApiUtil.attachErrorHandler(
     res,
-    AuthService.sendOtp(phoneInternational, typeOfUserToCreate).then(
-      (response) => {
-        const isOk = response.hasSent && !response.isWaiting;
-        let message = isOk
-          ? `Successfully sent OTP to ${phoneInternational}`
-          : "Failed to send OTP";
-        message = response.isWaiting
-          ? "Failed: You need to wait before you can send another OTP request"
-          : message;
-        const statusCode = isOk ? 200 : 403;
+    AuthService.sendOtp(user).then((response) => {
+      const isOk = response.hasSent && !response.isWaiting;
+      let message = isOk
+        ? `Successfully sent OTP to ${phoneInternational}`
+        : "Failed to send OTP";
+      message = response.isWaiting
+        ? "Failed: You need to wait before you can send another OTP request"
+        : message;
+      const statusCode = isOk ? 200 : 403;
 
-        const apiResponseData = {
-          phone,
-          ...response,
-        };
+      const apiResponseData = {
+        phone,
+        ...response,
+      };
 
-        const apiResponse = ApiUtil.getResponse(
-          isOk,
-          message,
-          apiResponseData,
-          statusCode
-        );
+      const apiResponse = ApiUtil.getResponse(
+        isOk,
+        message,
+        apiResponseData,
+        statusCode
+      );
 
-        ApiUtil.printResponse(res, apiResponse);
-      }
-    )
+      ApiUtil.printResponse(res, apiResponse);
+    })
   );
 };
 
