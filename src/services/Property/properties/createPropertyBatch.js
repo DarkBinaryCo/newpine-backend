77817319ddcfdc688e490cba4d5551ad55221c4c
Allan Jeremy <aj.dev254@gmail.com>
2021-10-09 14:47:46 +0300

    [Feature] Added ability to create and add multiple properties at the same time to property service

---
+++ src/services/Property/properties/createPropertyBatch.js
@@ -0,0 +1,34 @@
+const _ = require("lodash");
+// Models
+const { Community, Property } = require("../../../models");
+
+/** Create a new property with `insertData`
+ * @description This method also returns increments the number of properties that exist in the specified community
+ * @param {Array<Object>} insertData Data to enter into the database
+ * @param {UUID} communityId The id of the community these properties should belong to
+ * @return {Promise<Object>} A promise that resolves to an object with the create operation information
+ */
+const createPropertyBatch = async (insertData, communityId) => {
+  let propertySettableFields = [
+    "propertyNumber",
+    "propertyTypeId",
+    "propertyGroupId",
+    "communityId",
+  ];
+
+  // This comes first to catch any potential errors during insertion & get number of records inserted
+  const createdProperties = await Property.bulkCreate(insertData, {
+    fields: propertySettableFields,
+    returning: true,
+  });
+
+  await Community.increment(
+    { propertyCount: createdProperties.length },
+    { where: { id: communityId } }
+  );
+
+  return createdProperties;
+};
+
+//* EXPORTS
+module.exports = createPropertyBatch;
--- src/services/Property/properties/index.js
+++ src/services/Property/properties/index.js
@@ -4,16 +4,26 @@
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
 const getPhases = require("./getPhases");
+
+const createPropertyBatch = require("./createPropertyBatch");
 const getProperties = require("./getProperties");
+
 const getPropertyGroups = require("./getPropertyGroups");
+
 const getPropertyGroupTypes = require("./getPropertyGroupTypes");
+
 const getPropertyTypes = require("./getPropertyTypes");
 
 //* EXPORTS
 module.exports = {
   getPhases,
+
+  createPropertyBatch,
   getProperties,
+
   getPropertyGroups,
+
   getPropertyGroupTypes,
+
   getPropertyTypes,
 };
