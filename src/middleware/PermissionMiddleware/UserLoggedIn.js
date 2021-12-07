6330eb9b18d5d432ca644822de166e9971430994
AllanJeremy <aj.dev254@gmail.com>
2021-12-07 20:26:41 +0300

    [Refactoring] Converted user logged in middleware into a function that accepts an authorized user type param This means we no longer have to add a new middleware file for each user type we create - Also, removed other user type authentication files that were not performing any additional logic

--- src/middleware/PermissionMiddleware/UserLoggedIn.js
+++ src/middleware/PermissionMiddleware/UserLoggedIn.js
@@ -4,23 +4,38 @@ const { ApiUtil } = require("../../utils");
 const _getUserTokenData = require("./_getUserTokenData");
 
 /** [Helper] Only proceeds to the next middleware if a user of the specified user type is logged in based on the AuthToken passed in
- *
+ * @param {Number | Array<Number>} authorizedUserTypes The id of the authorized user types
  * Also sets user data on the `req.userData` which is passed on any middleware that comes after
  */
-const userLoggedIn = async (req, res, next) => {
-  const userTokenData = await _getUserTokenData(req);
+const userLoggedIn = (authorizedUserTypes = null) => {
+  return async (req, res, next) => {
+    const userTokenData = await _getUserTokenData(req);
+    console.log("Checking if user is logged in: ", userTokenData);
+    // Move to the next middleware if the user is logged in
+    if (userTokenData.loggedIn) {
+      req.userData = userTokenData.tokenData.user;
 
-  // Move to the next middleware if the user is logged in
-  if (userTokenData.loggedIn) {
-    // Set userData
-    req.userData = userTokenData.tokenData.user;
+      //* Getting here means we need to restrict the user's access by type
+      const currentUserType = req.userData.userTypeId;
+      const authorizedUserTypesIsArray = !!authorizedUserTypes?.length;
 
-    next();
-  } else {
-    // User is not logged in ~ show authorization error
-    let apiResponse = ApiUtil.getUnauthorizedError();
-    ApiUtil.printResponse(res, apiResponse);
-  }
+      //
+      const userIsAuthorized = authorizedUserTypesIsArray
+        ? authorizedUserTypes.includes(currentUserType)
+        : currentUserType === authorizedUserTypes;
+
+      //
+      if (!authorizedUserTypes || userIsAuthorized) {
+        next();
+      } else {
+        // User is not logged in to an account with sufficient permissions
+        ApiUtil.printResponse(res, ApiUtil.getUnauthorizedError());
+      }
+    } else {
+      // User is not logged in ~ show authorization error
+      ApiUtil.printResponse(res, ApiUtil.getUnauthorizedError());
+    }
+  };
 };
 
 //* EXPORTS
--- src/middleware/PermissionMiddleware/index.js
+++ src/middleware/PermissionMiddleware/index.js
@@ -2,21 +2,15 @@
     This file is simply meant to be an aggregator of all PermissionMiddleware related functions.
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
-const AdminLoggedIn = require("./AdminLoggedIn");
 const UserLoggedIn = require("./UserLoggedIn");
 const ResidentLoggedIn = require("./ResidentLoggedIn");
-const ResidentRepLoggedIn = require("./ResidentRepLoggedIn");
-const SecurityManagerLoggedIn = require("./SecurityManagerLoggedIn");
 const SecurityGuardLoggedIn = require("./SecurityGuardLoggedIn");
 const VisitorCanCheckin = require("./VisitorCanCheckin");
 
 //* EXPORTS
 module.exports = {
-  AdminLoggedIn,
   UserLoggedIn,
   ResidentLoggedIn,
-  ResidentRepLoggedIn,
-  SecurityManagerLoggedIn,
   SecurityGuardLoggedIn,
   VisitorCanCheckin,
 };
